6367aba56c61766d29c5763a8e929c11d1275c6d
ICarlos-18 <csanchezhernandez567@gmail.com>
2024-02-16 12:09:27 -0600

    Adding Theme

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -6,9 +6,21 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: const HomeScreen(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromARGB(95, 157, 194, 232),
+        appBarTheme: const AppBarTheme(color:  Color.fromARGB(95, 60, 62, 62)),
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle(
+            color: Color.fromARGB(255, 14, 18, 18),
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'sans-serif',
+          )
+        )
+      ),
     );
   }
 }
