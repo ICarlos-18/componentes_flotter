34c38ec8fa939b719dc20b350af7cbe28b9bb5b0
ICarlos-18 <csanchezhernandez567@gmail.com>
2024-02-20 13:42:22 -0600

    Finalizing the app theme

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -5,7 +5,9 @@ class AppTheme{
 
   // Declaraciones de colores
   static const mainColor = Color.fromARGB(225, 18, 9, 149);
-  static const backColor = Color.fromARGB(95, 160, 157, 232);
+  static const backColor = Color.fromARGB(95, 26, 180, 215);
+  static const accentcolor = Color.fromARGB(200, 17, 39, 204);
+
 
   // Declaraciones del Tema claro
   static final ThemeData lightTheme = ThemeData.light().copyWith(
@@ -22,7 +24,12 @@ class AppTheme{
         decorationThickness: 3.0,
         fontStyle: FontStyle.italic,
       ),
-      // bodySmall: 
+      bodySmall: GoogleFonts.acme(
+        color: accentcolor,
+        fontSize: 18.5,
+        fontWeight: FontWeight.w400,
+        fontStyle: FontStyle.normal
+      ),
     ),
   );
 }
